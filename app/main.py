2687efd5e602d0a2128f930807b61d18d91522c4
Dave Hurley <djmhur@gmail.com>
2024-03-20 02:47:25 +0000

    5th stage

--- app/main.py
+++ app/main.py
@@ -10,11 +10,24 @@ def main():
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
-            rid = b"\x04\xd2"
-            rflags = b"\x80\x00"
+            c_hdr = buf[:12]
+            c_id = c_hdr[:2]
+            c_flags = c_hdr[2:4]
+            qr = b"\x80\x00"
+            opcode = c_flags & b"\x78\x00"
+            aa = b"\x00\x00"
+            tc = b"\x00\x00"
+            rd = c_flags & b"\x01\x00"
+            ra = b"\x00\x00"
+            rsv = b"\x00\x00"
+            rcode = b"\x00\x00"
+            flags = qr|opcode|aa|tc|rd|ra|rsv|rcode
+            
             qdcount = b"\x00\x01"
             ancount = b"\x00\x01"
-            header = rid + rflags + qdcount + ancount + (b"\x00"*4)
+            nscount = b"\x00\x00"
+            arcount = b"\x00\x00"
+            header = c_id + flags + qdcount + ancount + nscount + arcount
             question = b"\x0ccodecrafters\x02io\x00\x00\x01\x00\x01"
             ttl = b"\x00\x00\x00\x3c"
             data = b"\x08\x08\x08\x08"
